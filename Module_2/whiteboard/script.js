50db1a72f430cfd1b106afbad23af93ba483c368
Sumant <sumant.sharma.nonu@gmail.com>
2021-06-16 19:24:06 +0530

    canvas

--- Module_2/whiteboard/script.js
+++ Module_2/whiteboard/script.js
@@ -1,9 +1,20 @@
 let canvas=document.querySelector("#canvas");
-canvas.style.width=window.innerWidth;
+canvas.style.width=;
+// canvas.
+
+// window.addEventListener("resize",function(){
+
+// })
 //a context object which provides fun for 2d drawing .
 let ctx=canvas.getContext("2d");
 
 console.log(ctx);
 
 ctx.fillStyle="yellow";
+ctx.fillRect(10,10,500,500);
+ctx.beginPath();
 
+ctx.moveTo(10,10);
+ctx.lineTo(100,250);
+ctx.lineTo(50,20);
+ctx.stroke();
