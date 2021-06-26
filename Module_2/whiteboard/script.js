4e6c28b81bf739ba90eb51914c511fc55bb92832
Sumant <sumant.sharma.nonu@gmail.com>
2021-06-27 00:59:01 +0530

    tool css

--- Module_2/whiteboard/script.js
+++ Module_2/whiteboard/script.js
@@ -4,8 +4,36 @@ canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 
 window.addEventListener("resize",function(){
-    canvas.width=this.window.innerWidth;
-    canvas.height=this.window.innerHeight-100;
+    canvas.width=window.innerWidth;
+    canvas.height=window.innerHeight-100;
 })
 
 
+let ctx=canvas.getContext("2d");
+
+let isPenDown=false;
+
+canvas.addEventListener("mousedown",function(e){
+ isPenDown=true;
+ let x=e.clientX;
+ let y=e.clientY;
+
+ ctx.beginPath();
+ ctx.moveTo(x,y);
+})
+
+
+canvas.addEventListener("mousemove",function(e){
+    
+    let x=e.clientX;
+    let y=e.clientY-100;
+   
+    
+    ctx.lineTo(x,y);
+    ctx.stroke();
+   })
+
+
+   canvas.addEventListener("mousedown",function(e){
+    isPenDown=false;
+     })
