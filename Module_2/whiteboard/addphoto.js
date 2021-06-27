2f11af39153654a8373e69ae724eca0ab9b72261
Sumant <sumant.sharma.nonu@gmail.com>
2021-06-27 11:26:27 +0530

    added photo

--- Module_2/whiteboard/addphoto.js
+++ Module_2/whiteboard/addphoto.js
@@ -1,1 +1,19 @@
+let photo=document.querySelector("#photo");
+let photoInput=document.querySelector("#photo-upload");
 
+
+photo.addEventListener("click",function(){
+    console.log("clickedOnphoto")
+    photoInput.click();
+})
+
+photoInput.addEventListener("change",function(e){
+  console.log("selectedaPhoto");
+    console.log(e);
+    let fileobject=e.target.files[0];
+    let  objectURL = window.URL.createObjectURL(fileobject,{type:"image/jpg"});
+    let img=document.createElement("img");
+    img.setAttribute("src",objectURL);
+    img.classList.add("sticky-image");
+    addStick(img);
+})
--- Module_2/whiteboard/sticky.js
+++ Module_2/whiteboard/sticky.js
@@ -2,21 +2,36 @@ let sticky=document.querySelector("#sticky");
 
 sticky.addEventListener("click",addStick);
 
-function addStick(){
+function addStick(imageElement){
     let stickyDiv=document.createElement("div");
     stickyDiv.classList.add("sticky");
     stickyDiv.innerHTML=`
     <div class="sticky-header">
     <div class="minimize"></div>
     <div class="close"></div>
-    </div>
-    <div class="sticky-content" contenteditable="true"></div>`;
+    </div>`;
 
-
+let stickyHeader=stickyDiv.querySelector(".sticky-header")
 let minimize=stickyDiv.querySelector(".minimize");
 let close=stickyDiv.querySelector(".close");
-let stickyContent=stickyDiv.querySelector(".sticky-content");
-let stickyHeader=stickyDiv.querySelector(".sticky-header");
+let stickyContent;
+if(imageElement){
+  let stickyImageDiv = document.createElement("div");
+  stickyImageDiv.classList.add("sticky-image-div");
+  stickyDiv.append(stickyImageDiv);
+  stickyImageDiv.append(imageElement);
+  stickyContent = stickyImageDiv;
+}else{
+   // <div class="sticky-content" contenteditable="true"></div>
+   let stickyContentDiv = document.createElement("div");
+   stickyContentDiv.classList.add("sticky-content");
+   stickyContentDiv.setAttribute("contenteditable" , "true");
+   stickyDiv.append(stickyContentDiv);
+   stickyContent = stickyContentDiv;
+}
+
+
+
 
 minimize.addEventListener("click",function(){
     stickyContent.style.display == "none"
