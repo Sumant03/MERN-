b7ece54ca61cd11a51c32d0aeccc01b31bf73292
Sumant <sumant.sharma.nonu@gmail.com>
2021-06-27 11:42:57 +0530

    downlaod implemented

--- Module_2/whiteboard/addphoto.js
+++ Module_2/whiteboard/addphoto.js
@@ -1,6 +1,6 @@
 let photo=document.querySelector("#photo");
 let photoInput=document.querySelector("#photo-upload");
-
+let download=document.querySelector("#download");
 
 photo.addEventListener("click",function(){
     console.log("clickedOnphoto")
@@ -16,4 +16,13 @@ photoInput.addEventListener("change",function(e){
     img.setAttribute("src",objectURL);
     img.classList.add("sticky-image");
     addStick(img);
+})
+
+download.addEventListener("click",function(){
+  let imagePath=canvas.toDataURL("image/jpg");
+let aTag=document.createElement("a");
+    aTag.download="canavs.jpg";
+    aTag.href=imagePath
+    aTag.click();
+
 })
--- Module_2/whiteboard/script.js
+++ Module_2/whiteboard/script.js
@@ -6,6 +6,7 @@ canvas.height=window.innerHeight;
 window.addEventListener("resize",function(){
     canvas.width=window.innerWidth;
     canvas.height=window.innerHeight-100;
+    drawLinesFromDB();
 })
 
 
