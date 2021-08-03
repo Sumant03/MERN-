5ce6a817619382e6dce9ad4b673a24435de8a241
Sumant <sumant.sharma.nonu@gmail.com>
2021-08-03 18:28:28 +0530

    react started

--- demp/src/index.js
+++ demp/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';  //creation logic
+import ReactDOM from 'react-dom'; //render ogic
 
+
+//App is a component we have imported
 import App from './App';
+
 
 ReactDOM.render(
   
-    <App />,
-  
-  document.getElementById('root')
-);
+    <App />,  //ek component
+     document.getElementById('root')
+              );
