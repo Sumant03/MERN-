61bfe7cbff1b7ba9073dc3ae707fd7b56d1cdfac
Sumant <sumant.sharma.nonu@gmail.com>
2021-08-09 21:51:13 +0530

    child work done

--- Module_3/Movies/movies/src/Child.jsx
+++ Module_3/Movies/movies/src/Child.jsx
@@ -2,9 +2,38 @@ import React from "react";
 import { ReactDOM } from "react";
 
 class Child extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("Constructor Called");
+        this.state={
+            on:false,
+        }
+    }
+
+    componentDidMount(){
+        console.log("component did mount was called");
+    }
+
+    componentDidUpdate(){
+        console.log("component updated");
+    }
+    componentWillUnmount(){
+        console.log("component is just deleting");
+    }
+
+    
+    
+    
     render(){
+        console.log("render was called");
         return(
-            <button>Click</button>
+            <button 
+            onClick={()=>{if(this.state.on){
+                 this.setState({on:false})
+            }else{
+                this.setState({on:true})
+            }}
+        }>Click</button>
         )
     }
 }
