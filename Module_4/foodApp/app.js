c60e4ae7ade0bcdea6dc9a46bce89d20942435b0
Sumant <sumant.sharma.nonu@gmail.com>
2021-09-13 20:01:37 +0530

    basic server done

--- Module_4/foodApp/app.js
+++ Module_4/foodApp/app.js
@@ -1,11 +1,20 @@
 const express=require('express');
+
+//server creation
 const app=express();
 
-let port=8080
+let port='8080';
 app.listen(port,function(){
     console.log('hi there server started');
 });
 
-// app.get('/',(req,res)=>{
+//types of request -> get post put delete
 
-// })
+app.get('/',(req,res)=>{
+    console.log(req);
+    console.log(req.hostname);
+    console.log(req.path);
+    console.log(req.method);
+res.send('<h1>hello hi</h1>');
+res.end();
+})
