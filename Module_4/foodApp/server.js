c14cbf8de3ff76cb07979587e99df963fc93c391
Sumant <sumant.sharma.nonu@gmail.com>
2021-09-15 14:01:55 +0530

    signup

--- Module_4/foodApp/server.js
+++ Module_4/foodApp/server.js
@@ -10,93 +10,112 @@ app.listen(port,function(){
 });
 
 app.use(express.json());
+app.use(express.static('public'))
+
+// const userRouter=express.Router();
+const authRouter=express.Router();
+// app.use('/user',userRouter);
+app.use('/auth',authRouter);
 
 
-const userRouter=express.Router();
-app.use('/user',userRouter);
 
-userRouter
-.route('/')
-.get(getUser)
-.post(createUser)
-.patch(updateUser)
-.delete(deleteUser)
+// userRouter
+// .route('/')
+// .get(getUser)
+// .post(createUser)
+// .patch(updateUser)
+// .delete(deleteUser)
 
-userRouter
-.route('/:id')
-.get(getUserById);
+// userRouter
+// .route('/:id')
+// .get(getUserById);
 
+
+authRouter
+.route('/signup')
+.post(signupUser)
 
 //types of request -> get post put delete
 
-app.get('/',(req,res)=>{
-    console.log(req);
-    console.log(req.hostname);
-    console.log(req.path);
-    console.log(req.method);
-res.send('<h1>server 5000</h1>');
+function signupUser(req,res){
 
-})
-let obj={}
+    let {email,name,password}=req.body;
+    user.push({email,name,password});
+    console.log('user',req.body);
+    res.json({
+        message:'user signup',
+        user:req.body
+    })}
 
-// app.get('/user',(req,res)=>{
+
+// app.get('/',(req,res)=>{
+//     console.log(req);
+//     console.log(req.hostname);
+//     console.log(req.path);
+//     console.log(req.method);
+// res.send('<h1>server 5000</h1>');
+
+// })
+// let obj={}
+
+// // app.get('/user',(req,res)=>{
     
-// res.send(obj);
-//   console.log(req.body);
-// })
+// // res.send(obj);
+// //   console.log(req.body);
+// // })
 
-function getUser(req,res){
-    res.json(user);
-}
-let user={};
-// app.post('/user',(req,res)=>{
+// function getUser(req,res){
+//     res.json(user);
+// }
+// let user={};
+// // app.post('/user',(req,res)=>{
+// //     user=req.body;
+// //     console.log(req.body);
+// //     res.send('data has been added')
+// // })
+
+// function createUser(req,res){
 //     user=req.body;
-//     console.log(req.body);
-//     res.send('data has been added')
-// })
+//     // console.log(req.body);
+//     res.send('data has been added succesfully');
+// }
 
-function createUser(req,res){
-    user=req.body;
-    // console.log(req.body);
-    res.send('data has been added succesfully');
-}
+// // app.patch('/user',(req,res)=>{
+// //     let obj=req.body;
+// //     for(let key in obj){
+// //         user[key]=obj[key];
 
-// app.patch('/user',(req,res)=>{
+// //     }
+// //     console.log(req.body);
+// //     console.log(user);
+// //     res.json(user)
+// // })
+
+// function updateUser (req,res){
 //     let obj=req.body;
 //     for(let key in obj){
 //         user[key]=obj[key];
-
 //     }
-//     console.log(req.body);
-//     console.log(user);
+//     res.json(user);
+// };
+
+// function deleteUser(req,res){
+//     user={};
+//     res.json(user);
+//     // res.send('ussr has been deleted');
+// }
+
+// function getUserById(req,res){
+//     console.log(req.params);
+//     res.json(req.params.id);
+// }
+
+// app.delete('/user',(req,res)=>{
+//     user={};
 //     res.json(user)
 // })
 
-function updateUser (req,res){
-    let obj=req.body;
-    for(let key in obj){
-        user[key]=obj[key];
-    }
-    res.json(user);
-};
-
-function deleteUser(req,res){
-    user={};
-    res.json(user);
-    // res.send('ussr has been deleted');
-}
-
-function getUserById(req,res){
-    console.log(req.params);
-    res.json(req.params.id);
-}
-
-app.delete('/user',(req,res)=>{
-    user={};
-    res.json(user)
-})
-
-app.get('/user/:id',(req,res)=>{
-    console.log(req.params);
-    res.json(req.params.id)
-})
+// app.get('/user/:id',(req,res)=>{
+//     console.log(req.params);
+//     res.json(req.params.id)
+// })
