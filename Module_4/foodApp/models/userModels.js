4862d2a287613b4c4a43739782ef59f8465c46fb
Sumant <sumant.sharma.nonu@gmail.com>
2021-09-20 20:04:10 +0530

    validated the user

--- Module_4/foodApp/models/userModels.js
+++ Module_4/foodApp/models/userModels.js
@@ -1,9 +1,59 @@
 const mongoose=require("mongoose");
-const db_link="mongodb+srv://Sumant:HiSNmfAtutUppmlp@firsttimebuildingdataba.h2wyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const db=require('../secrets');
+const validator=require('email-validator');
 
-mongoose.connect(db_link).then(function(){
+
+mongoose.connect(db.link).then(function(){
 console.log('db connected');
 })
 .catch(function(err){
 console.log(err);
 })
+
+const userSchema= new mongoose.Schema({
+  name:{
+      type:String,
+      required:true
+  },
+  email:{
+      type:String,
+      required:true,
+      unique:true,
+      validate:function(){
+       return validator.validate(this.email);
+      }
+  },  password:{
+    type:String,
+    required:true,
+
+    
+    min:8
+
+},  confirmpassword:{
+    type:String,
+    required:true,
+    min:8,
+    validate:function(){
+        return this.password=this.confirmpassword
+    }
+}
+})
+
+
+const userModel=mongoose.model('userModel',userSchema);
+
+
+
+(async function createUser(){
+    let user={
+        name:'Sumo',
+        age:20,
+        email:'abc@gmail.com',
+        password:'12345',
+        confirmpassword:'12345'
+    };
+    let userObj=await userModel.create(user);
+    console.log(userObj);
+})();
+
+
--- Module_4/resources.md
+++ Module_4/resources.md
@@ -1,9 +1,7 @@
 https://expressjs.com/en/guide/using-middleware.html
  
- Hello Libertad Bernal
 
-I feel great to connect with you. I saw a Back End Developer opening in your firm, I request you to kindly consider my application for the opening. My skills are 
-React, Express.js, Javascript, Firebase, DBMS.
 
-I hope for a peaceful reply from your side.
-Thank you
+
+
+validator npm for validation
