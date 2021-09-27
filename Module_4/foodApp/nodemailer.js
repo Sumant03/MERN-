7e8eb424c48351d253a1ec2df3e7f61c07239985
Sumant <sumant.sharma.nonu@gmail.com>
2021-09-27 23:18:23 +0530

    up

--- Module_4/foodApp/nodemailer.js
+++ Module_4/foodApp/nodemailer.js
@@ -1,1 +1,12 @@
 const nodemailer=require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
