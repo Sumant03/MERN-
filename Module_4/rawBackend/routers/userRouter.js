1457c56a760823f3f7b130dd8a7fafbb323aa66e
Sumant <sumant.sharma.nonu@gmail.com>
2021-12-19 00:21:53 +0530

    crud operations

--- Module_4/rawBackend/routers/userRouter.js
+++ Module_4/rawBackend/routers/userRouter.js
@@ -6,28 +6,95 @@ const jwt = require('jsonwebtoken');
 const {bodyChecker,protectRoute}=require("./myMiddleWare")
 userRouter
      .route('/')
-
-     .get(protectRoute,getUser)
+     .get(protectRoute,getUsers)
      .post(bodyChecker,createUser);
 
-function createUser(req,res){
-        console.log("create user");
-    
-        let body=req.body;
-    
-        console.log(body);
-        content.push(body);
-    
-        fs.writeFileSync(".data.json",JSON.stringify(content));
-        res.json({message:content});
+
+userRouter
+.route(":/id")
+    .get(getUser)
+    .post(bodyChecker,updateUser)
+    .delete(bodyChecker,deleteUser)
+
+async function createUser(req,res){
+      
+    try{
+        let user=await userModel.create(req.body);
+        res.status(200).json({
+            "message":"user added in database",
+            data:user
+        })
+
+    }catch(err){
+        res.status(404).json({
+            "message":err.message
+        })
     }
-async function getUser(req,res){
+    }
+async function getUsers(req,res){
         let user=await userModel.find();
       try{
         res.status(200).json({
             user:user
         })
     }catch(err){
+        res.status(404).json({
+            "message":err.message
+        })
+    }
+    }
+async function getUser(req,res){
+    let {id}=req.params;
+    try{
+        let user=await userModel.findById(id);
+        res.status(200).json({
+            "message":"user found in database",
+            data:user
+        })
+
+    }
+    catch(err){
+        res.status(404).json({
+            "message":err.message
+        })
+    }
+    
+    }
+
+async function updateUser(req,res){
+    let {id} =req.params;
+    try{
+
+        let user=await userModel.findById(id);
+        if(user){
+           
+            for(let key in user){
+                user[key]=req.body[key];
+            }
+            await user.save();
+            res.send(200).json({
+                "message":"data updated",
+                data:user
+            })
+        }
+
+    }
+    catch(err){
+        res.status(404).json({
+            "message":err.message
+        })
+    }
+}
+async function deleteUser(req,res){
+    let {id} =req.params;
+    try{
+         let user=await userModel.findByIdAndDelete(id);
+         res.status(200).json({
+             "message":"user data deleted ",
+             data:user
+         })
+    }
+    catch(err){
         res.status(404).json({
             "message":err.message
         })
